ff1fcb8ffae0587d37589959f82af70d49090f03
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-05-03 21:57:40 +0200

    First commit

---
+++ src/main/java/com/ffivrpportal/FfivRoleplayPortalApplication.java
@@ -0,0 +1,13 @@
+package com.ffivrpportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfivRoleplayPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FfivRoleplayPortalApplication.class, args);
+    }
+
+}
