263c81a4dacc887973dd5970f35a2f91ce08a9f5
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-05-11 15:30:50 +0200

    login is now functional

--- src/main/java/com/ffivrpportal/controller/SignInController.java
+++ src/main/java/com/ffivrpportal/controller/SignInController.java
@@ -1,16 +1,55 @@
 package com.ffivrpportal.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@RequestMapping("/sign-in")
 public class SignInController {
 
-    @GetMapping
-    public String displayPage() {
+    final String REDIRECT_USER = "redirect:/user/home";
+    final String REDIRECT_ADMIN = "redirect:/admin/home";
+    private final Authentication AUTHENTICATION = SecurityContextHolder.getContext().getAuthentication();
+
+    private String authenticate(Authentication authentication) {
+        if (authentication != null && authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
+            return REDIRECT_USER;
+        }
+
+        if (authentication != null && authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            return REDIRECT_ADMIN;
+        }
         return "sign-in";
+    }
+
+
+    @GetMapping("/sign-in")
+    public String loginPage() {
+
+        return authenticate(AUTHENTICATION);
+    }
+
+
+    @GetMapping("/sign-in/error")
+    public String loginWithError(Model model) {
+
+        model.addAttribute("loginError", true);
+        return "sign-in";
+    }
+
+    @GetMapping("/sign-out")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+
+        if (AUTHENTICATION != null) {
+            new SecurityContextLogoutHandler().logout(request, response, AUTHENTICATION);
+        }
+        return "redirect:/";
     }
 
 }
