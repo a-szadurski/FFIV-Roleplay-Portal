54a63a2c853f1f75d65fb343d768a1e1cb9d0d4c
a-szadurski <szadurski.arkadiusz@gmail.com>
2021-05-18 15:38:50 +0200

    user profile page placeholder, delete user home, code optimize, add th namespace where missing

---
+++ src/main/java/com/ffivrpportal/controller/user/UserProfileController.java
@@ -0,0 +1,23 @@
+package com.ffivrpportal.controller.user;
+
+import com.ffivrpportal.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/profile")
+public class UserProfileController {
+
+    private final UserService userService;
+
+    public UserProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String displayPage(Model model) {
+        return "user/profile";
+    }
+}
